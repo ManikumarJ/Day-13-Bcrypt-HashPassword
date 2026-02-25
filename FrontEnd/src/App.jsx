220c2c24a65c1f9a5dc764e465de89b0241d2353
ManikumarJ <manikumarj6@gmail.com>
2026-02-25 21:49:36 +0530

    Updated

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
 
-export const App = () => {
+const App = () => {
+
+  const studentSchema = {
+    UserName: "",
+    email: "",
+    Password: "",
+  };
+
+  const [data, setData] = useState(studentSchema);
+  const [students, setStudents] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post( "http://localhost:5000/api/UserDetails/create", data);
+      alert(res.data.msg);
+      setData(studentSchema);
+     
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-  <>
-    <div>
-      <form>
-        
+    <>
+      <h1 style={{ textAlign: "center" }}>Register Form</h1>
+
+      <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
+
+        <input type="text" name="UserName" placeholder="UserName" value={data.UserName} onChange={handleChange} required />
+        <br /><br />
+
+        <input type="email" name="email" placeholder="Email - Id" value={data.email} onChange={handleChange} required />
+        <br /><br />
+
+
+        <input type="password" name="Password" placeholder="Password" value={data.Password} onChange={handleChange} required />
+        <br /><br />
+
+        <button type="submit">Submit</button>
+
       </form>
-    </div>
-  </>
-  )
-}
+
+    </>
+  );
+};
+
+export default App;
